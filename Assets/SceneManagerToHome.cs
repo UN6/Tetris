0dfa305f1052c70f3eee207d59cb36980d33bbd9
UN6 <apple.ktd@icloud.com>
2019-11-24 18:13:10 +0900

    音楽、シーン遷移、スコアの表示、ボタンのUIの追加

---
+++ Assets/SceneManagerToHome.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerToHome : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ToHome()　//タイトル（サンプルシーン）に戻る
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
---
+++ Assets/ScoreDirector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDirector : MonoBehaviour
+{
+    GameObject scoreText;
+    int score = 500; //ゲームシーンからスコアを持ってくる
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.scoreText = GameObject.Find("Score");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.scoreText.GetComponent<Text>().text = this.score.ToString();
+    }
+}
